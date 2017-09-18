97a381c268550fc828c148f6d9a0aacdbe34d371
Gus Nguyen <nguyenhachuy@gmail.com>
2017-09-17 18:50:50 -0700

    Added game setups

--- javascript/game.js
+++ javascript/game.js
@@ -10,11 +10,25 @@ $(`.gem`).on("click", function() {
 
 
 function updateScore(value) {
-	$(".score").text(value);
+	$(".total-score").text(value);
 }
 // Populate power with randoms 
 for(var i = 0; i < 4; i++){
-	power.push(Math.floor((Math.random() * 12)));
+	power.push(getRandomInt(1,12));
 	//Attach values dynamically
 	$(`.crystal-${i}`).data("value", power[i]);
 }
+
+function setupGame() {
+	$(".score").text(getRandomInt(19, 120));
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+$(document).ready(function() {
+	setupGame();
+});
