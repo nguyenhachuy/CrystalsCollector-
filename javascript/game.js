7395674e246a8df593953092705c7f5fe8a3eb47
Gus Nguyen <nguyenhachuy@gmail.com>
2017-09-17 18:20:07 -0700

    Added random power

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,11 @@
-for(var i = 0; i < 4; i++){
-	$(`.crystal${i}`).on("click", function() {
+var power = [];
+
+$(`.crystal${i}`).on("click", function() {
 	alert("yo");
 });
+
+
+// Populate power with randoms 
+for(var i = 0; i < 4; i++){
+	power.push(Math.floor((Math.random() * 12)));
 }
