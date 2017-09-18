56cb549e4143ef8c07c1bd699cf7e1b97dfc7051
Gus Nguyen <nguyenhachuy@gmail.com>
2017-09-17 18:42:16 -0700

    Score is now reflected

--- javascript/game.js
+++ javascript/game.js
@@ -1,11 +1,20 @@
 var power = [];
-
-$(`.crystal${i}`).on("click", function() {
-	alert("yo");
+var score = 0;
+$(`.gem`).on("click", function() {
+	//This is how to get the value fo the crystal
+	var value = $(this).data("value");
+	console.log(value);
+	score += value;
+	updateScore(score);
 });
 
 
+function updateScore(value) {
+	$(".score").text(value);
+}
 // Populate power with randoms 
 for(var i = 0; i < 4; i++){
 	power.push(Math.floor((Math.random() * 12)));
+	//Attach values dynamically
+	$(`.crystal-${i}`).data("value", power[i]);
 }
