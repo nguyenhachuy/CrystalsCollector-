f5c8c46e10af8436819252a93795264c43282c70
Gus Nguyen <nguyenhachuy@gmail.com>
2017-09-17 19:08:48 -0700

    Added game setup

--- javascript/game.js
+++ javascript/game.js
@@ -1,26 +1,23 @@
 var power = [];
 var score = 0;
-$(`.gem`).on("click", function() {
-	//This is how to get the value fo the crystal
-	var value = $(this).data("value");
-	console.log(value);
-	score += value;
-	updateScore(score);
-});
 
 
 function updateScore(value) {
 	$(".total-score").text(value);
 }
-// Populate power with randoms 
-for(var i = 0; i < 4; i++){
-	power.push(getRandomInt(1,12));
-	//Attach values dynamically
-	$(`.crystal-${i}`).data("value", power[i]);
-}
 
 function setupGame() {
 	$(".score").text(getRandomInt(19, 120));
+}
+
+function setupGem() {
+	// Populate power with randoms 
+	for(var i = 0; i < 4; i++){
+		power.push(getRandomInt(1,12));
+		//Attach values dynamically
+		$(`.crystal-${i}`).data("value", power[i]);
+	}
+
 }
 
 function getRandomInt(min, max) {
@@ -29,6 +26,28 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+function checkGameCondition() {
+	var score = parseInt($(".total-score").text());
+	var goal = parseInt($(".score").text());
+	//Check losing condition 
+	if(score > goal) return -1;
+	//Check winning condition
+	else if(score === goal) return 1;
+	//Indicates game is still going on
+	else return 0;
+}
+
 $(document).ready(function() {
 	setupGame();
+	setupGem();
+	//Adding listeners to crystals
+	$(`.gem`).on("click", function() {
+		//This is how to get the value fo the crystal
+		var value = $(this).data("value");
+		score += value;
+		updateScore(score);
+		console.log(checkGameCondition());		
+	});
+
+
 });
